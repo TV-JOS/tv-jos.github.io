6902ced50f3ea302ae8637c6ae4eb35e71fccaeb
TV-JOS <163703215+TV-JOS@users.noreply.github.com>
2024-03-17 02:09:35 -0300

    Add files via upload

--- tv.js
+++ tv.js
@@ -3,3 +3,8 @@ const onBeforeUnload = () => {
 }
 
 window.addEventListener("beforeunload", onBeforeUnload, false);
+
+if (!localStorage.getItem('visited') && window.location.pathname !== 'index.html') {
+    localStorage.setItem('visited', true);
+    window.location.href = 'index.html';
+}
